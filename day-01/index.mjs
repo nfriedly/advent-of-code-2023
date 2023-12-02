8004a613e63717e3058b705ec62947323ab2f6cf
Nathan Friedly <nathan@nfriedly.com>
2023-12-01 22:29:44 -0500

    day 1, part 2 quick and dirty, but good enough for this input

--- day-01/index.mjs
+++ day-01/index.mjs
@@ -9,6 +9,34 @@ function parse(input) {
     ))
 }
 
+function toNum(numWord) {
+    return {
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
+    }[numWord] || numWord
+}
+
+function reverse(str) {
+    return str.split("").reverse().join("")
+}
+
+function parseLine(line) {
+    const a = line.match(/[0-9]|one|two|three|four|five|six|seven|eight|nine/)[0];
+    const b = reverse(reverse(line).match(/[0-9]|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin/)[0])
+    return parseInt(`${toNum(a)}${toNum(b)}`, 10)
+}
+
+function parseTwo(input) {
+    return input.split(/[\r\n]+/g).map(parseLine)
+}
+
 function add(nums) {
     return nums.reduce((a,b) => a+b)
 }
@@ -16,6 +44,10 @@ function add(nums) {
 function part1() {
     console.log('day 1, part 1: ', add(parse(input)))
 }
+function part2() {
+    console.log('day 1, part 2: ', add(parseTwo(input)))
+}
+
 
 
 const testInput = `1abc2
@@ -25,4 +57,16 @@ treb7uchet`;
 assert.deepEqual(parse(testInput), [12, 38, 15, 77])
 assert.equal(add(parse(testInput)), 142)
 
+const testTwoInput = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`
+
+assert.deepEqual(parseTwo(testTwoInput), [29, 83, 13, 24, 42, 14, 76])
+assert.equal(add(parseTwo(testTwoInput)), 281)
+
 part1()
+part2()
