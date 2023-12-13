a120d89e574e51b11483886451db072114348204
Nathan Friedly <nathan@nfriedly.com>
2023-12-13 17:42:36 -0500

    Day 13, part 2

--- day-13/index.mjs
+++ day-13/index.mjs
@@ -1,6 +1,6 @@
 import assert, { AssertionError } from "node:assert";
 import fs from 'node:fs'
-import { toColumns, lines, sections, sum } from '../utils.mjs'
+import { toColumns, toRows, sections, sum } from '../utils.mjs'
 import _ from 'lodash';
 
 const testInput = `#.##..##.
@@ -22,30 +22,48 @@ const testInput = `#.##..##.
 function parse(input) {
     return sections(input).map(section => ({
         // rows and cols are each an array of strings
-        rows: lines(section), 
-        cols: toColumns(section).map(c => c.join(''))})
+        rows: toRows(section), 
+        cols: toColumns(section)})
     );
 }
 
 assert.deepEqual(parse('12\n34'), [{
-    rows: ['12', '34'],
-    cols: ['13', '24']
+    rows: [['1','2'], ['3','4']],
+    cols: [['1','3'], ['2','4']]
 }])
 
+function diff(rowa, rowb) {
+    let differences = 0;
+    rowa.forEach((a,i) => {
+        if (a != rowb[i]) {
+            differences++;
+        }
+    });
+    return differences;
+}
+
+assert.equal(diff(['.','#','.'], ['.','#','.']), 0)
+assert.equal(diff(['.','#','.'], ['.','#','.']), 0)
+assert.equal(diff(['.','#','.'], ['.','#','#']), 1)
+assert.equal(diff(['.','#','.'], ['.','#','#']), 1)
+assert.equal(diff(['.','#','.'], ['#','#','#']), 2)
+
 // returns the index of the row or column just after the mirror line
-function findMirrorLine(data) {
+function findMirrorLine(data, smudges=0) {
     // start at the second line
     const len = data.length;
     outer: for(let mirrorLine=1; mirrorLine<len; mirrorLine++) {
-        if (data[mirrorLine-1] == data[mirrorLine]) {
+        let differences = diff(data[mirrorLine-1], data[mirrorLine]);
+        if (differences <= smudges) {
             // iterate outwards until reaching the end of the array
             const distance = Math.min(mirrorLine, len-mirrorLine)
             for (let offset=1; offset<distance; offset++) {
-                if (data[mirrorLine+offset] != data[mirrorLine-1-offset]) {
+                differences += diff(data[mirrorLine+offset], data[mirrorLine-1-offset])
+                if (differences > smudges) {
                     continue outer;
                 }
             }
-            return mirrorLine
+            if (differences == smudges) return mirrorLine
         }
     }
     return 0;
@@ -57,8 +75,8 @@ assert.equal(findMirrorLine(testData[0].cols), 5)
 assert.equal(findMirrorLine(testData[1].rows), 4)
 assert.equal(findMirrorLine(testData[1].cols), 0)
 
-function summarize(sections) {
-    return sum(sections.map(s => findMirrorLine(s.rows)*100 + findMirrorLine(s.cols)))
+function summarize(sections, smudges=0) {
+    return sum(sections.map(s => findMirrorLine(s.rows, smudges)*100 + findMirrorLine(s.cols, smudges)))
 }
 
 assert.equal(summarize(testData), 405)
@@ -66,3 +84,10 @@ assert.equal(summarize(testData), 405)
 const input = fs.readFileSync('./input.txt').toString();
 const data = parse(input);
 console.log('Part 1:', summarize(data))
+
+assert.equal(findMirrorLine(testData[0].rows, 1), 3)
+assert.equal(findMirrorLine(testData[0].cols, 1), 0)
+assert.equal(findMirrorLine(testData[1].rows, 1), 1)
+assert.equal(findMirrorLine(testData[1].cols, 1), 0)
+
+console.log('Part 1:', summarize(data, 1))
